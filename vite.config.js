73c30a63d98a3a37d4f6892436417b3c27502c48
Tu Dinh <tudinh@Tus-MacBook-Pro.wifi.gsu.edu>
2024-03-18 15:17:41 -0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/tudinh411.github.io/",
+  base:"/tudinh411.github.io",
   plugins: [react()],
   css: {
     modules: {
